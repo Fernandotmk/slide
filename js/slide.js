d6ae488c84e7b2388052fdef6701080de45fcd4b
“Fernandotmk” <fernando.tmk@hotmail.com>
2022-06-25 11:04:05 -0300

    calculo de distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,22 +2,47 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    // salvando uma referencia do valor do eixo x ao mover, para que nao volte a 0
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clienteX) {
+    // agora pega o eixo x definido abaixo
+    this.dist.movement = (this.dist.startX - clienteX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
-    this.wrapper.addEventListener('mousedown', this.onMove);
+    // define o eixo x como sendo igual ao eixo x do evento ao clicar
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
-  onMove(event) {}
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousedown', this.onMove);
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
+    // ativa função onStart com o clique fica segurado
     this.wrapper.addEventListener('mousedown', this.onStart);
-    this.wrapper.addEventListener('mousedown', this.onEnd);
+    // ativa função onEnd quando solta o clique do mouse
+    this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents() {
